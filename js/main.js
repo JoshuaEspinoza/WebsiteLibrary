a03b10838e0e81a309e048ba9069fc48b5682566
Josue Espinoza <143584189+JoshuaEspinoza@users.noreply.github.com>
2023-11-28 03:09:37 -0600

    Header finalizado.

---
+++ js/main.js
@@ -0,0 +1,43 @@
+/*=============== SEARCH ===============*/
+
+const searchButton = document.getElementById('search-button'),
+	searchClose = document.getElementById('search-close'),
+	searchContent = document.getElementById('search-content');
+
+/*=============== SEARCH SHOW ===============*/
+
+if (searchButton) {
+	searchButton.addEventListener('click', () => {
+		searchContent.classList.add('show-search');
+	});
+}
+
+/*=============== SEARCH HIDDEN ===============*/
+
+if (searchClose) {
+	searchClose.addEventListener('click', () => {
+		searchContent.classList.remove('show-search');
+	});
+}
+
+/*=============== LOGIN ===============*/
+
+const loginButton = document.getElementById('login-button'),
+	loginClose = document.getElementById('login-close'),
+	loginContent = document.getElementById('login-content');
+
+/*=============== LOGIN SHOW ===============*/
+
+if (loginButton) {
+	loginButton.addEventListener('click', () => {
+		loginContent.classList.add('show-login');
+	});
+}
+
+/*=============== LOGIN HIDDEN ===============*/
+
+if (loginClose) {
+	loginClose.addEventListener('click', () => {
+		loginContent.classList.remove('show-login');
+	});
+}
